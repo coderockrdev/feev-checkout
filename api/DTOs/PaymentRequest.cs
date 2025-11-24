5d69182434a3bde31d53d01a2e640517c8df07db
Daniel Freitas <git@scrlkx.com>
2025-11-24 15:48:13 -0300

    feat: restrict installments based on method

--- api/DTOs/PaymentRequest.cs
+++ api/DTOs/PaymentRequest.cs
@@ -8,6 +8,19 @@ public record PaymentRequest(
     [Required(ErrorMessage = "Method is required.")]
     [EnumDataType(typeof(PaymentMethod), ErrorMessage = "Method not supported.")]
     PaymentMethod Method,
-    [Required(ErrorMessage = "Installments is required.")]
-    int Installments
-);
+    int? Installments
+) : IValidatableObject
+{
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Method == PaymentMethod.FeevBoleto || Method == PaymentMethod.FeevPix)
+            if (Installments != null)
+                yield return new ValidationResult($"Installments are not allowed for '{Method}'.",
+                    [nameof(Installments)]);
+
+        if (Method == PaymentMethod.FeevPix)
+            if (Installments == null)
+                yield return new ValidationResult("Installments are required.",
+                    [nameof(Installments)]);
+    }
+}
