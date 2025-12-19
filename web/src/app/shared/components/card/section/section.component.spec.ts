d5afc29b975363b9abe00246ea64cb59dcd293b6
Nicolas A. Feitoza <afxnicolas@gmail.com>
2025-12-19 18:08:05 -0300

    feat(wip): integrate transaction data and include credit card form

--- web/src/app/shared/components/card/section/section.component.spec.ts
+++ web/src/app/shared/components/card/section/section.component.spec.ts
@@ -3,22 +3,21 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SectionComponent } from "./section.component";
 
-describe('SectionComponent', () => {
+describe("SectionComponent", () => {
   let component: SectionComponent;
   let fixture: ComponentFixture<SectionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SectionComponent]
-    })
-    .compileComponents();
+      imports: [SectionComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
