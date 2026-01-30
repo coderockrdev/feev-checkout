86d90733fbfb07704933d5feacacfee4599842cb
Nicolas A. Feitoza <afxnicolas@gmail.com>
2026-01-30 13:28:12 -0300

    feat(payments): rename from fatura to boleto

--- web/src/app/modules/checkout/constants/payment-methods-options.ts
+++ web/src/app/modules/checkout/constants/payment-methods-options.ts
@@ -1,5 +1,5 @@
 export const PAYMENT_METHODS_OPTIONS = [
   { label: "PIX", value: "feev_pix", icon: "pix" },
   { label: "Cartão de Crédito", value: "braspag_cartao", icon: "credit-card" },
-  { label: "Fatura", value: "feev_boleto", icon: "barcode" },
+  { label: "Boleto", value: "feev_boleto", icon: "barcode" },
 ] as const;
